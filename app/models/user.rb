5c10db6871ec45ace2cef0f03eecfcde2bd5e009
Alesh Houdek <mail@alesh.com>
2015-08-09 08:29:42 -0400

    8.4.1 Model changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  attr_accessor :remember_token
   before_save { self.email = email.downcase }
   validates :name,  presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 },
@@ -14,5 +15,13 @@ class User < ActiveRecord::Base
     BCrypt::Password.create(string, cost: cost)
   end
 
+def User.new_token
+  SecureRandom.urlsafe_base64
 end
 
+def remember
+  self.remember_token = User.new_token
+  update_attribute(:remember_digest, User.digest(remember_token))
+end
+
+end
